530d6fa26db0ebd52a1ae4e2c52e8d7d06835a4c
Usuario <Usuario@CPD02-M08>
2024-08-06 11:19:40 -0300

    Adicionar arquivos de projeto.

---
+++ Winformsgastos/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace Winformsgastos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbGastos = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbTv = new System.Windows.Forms.TextBox();
+            this.tbAgua = new System.Windows.Forms.TextBox();
+            this.tbComida = new System.Windows.Forms.TextBox();
+            this.tbOutros = new System.Windows.Forms.TextBox();
+            this.tbEnergia = new System.Windows.Forms.TextBox();
+            this.tbRenda = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblSaldo = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.gbGastos.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbGastos
+            // 
+            this.gbGastos.Controls.Add(this.label6);
+            this.gbGastos.Controls.Add(this.label5);
+            this.gbGastos.Controls.Add(this.label4);
+            this.gbGastos.Controls.Add(this.label3);
+            this.gbGastos.Controls.Add(this.label2);
+            this.gbGastos.Controls.Add(this.tbTv);
+            this.gbGastos.Controls.Add(this.tbAgua);
+            this.gbGastos.Controls.Add(this.tbComida);
+            this.gbGastos.Controls.Add(this.tbOutros);
+            this.gbGastos.Controls.Add(this.tbEnergia);
+            this.gbGastos.Location = new System.Drawing.Point(27, 125);
+            this.gbGastos.Name = "gbGastos";
+            this.gbGastos.Size = new System.Drawing.Size(399, 499);
+            this.gbGastos.TabIndex = 0;
+            this.gbGastos.TabStop = false;
+            this.gbGastos.Text = "Gastos";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(36, 410);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(113, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Outros gastos:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(48, 319);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Alimentação:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(98, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Água:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(19, 231);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Tv/Fone/Internet:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Energia elétrica:";
+            // 
+            // tbTv
+            // 
+            this.tbTv.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbTv.Location = new System.Drawing.Point(155, 221);
+            this.tbTv.Name = "tbTv";
+            this.tbTv.Size = new System.Drawing.Size(199, 35);
+            this.tbTv.TabIndex = 5;
+            // 
+            // tbAgua
+            // 
+            this.tbAgua.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbAgua.Location = new System.Drawing.Point(155, 130);
+            this.tbAgua.Name = "tbAgua";
+            this.tbAgua.Size = new System.Drawing.Size(199, 35);
+            this.tbAgua.TabIndex = 4;
+            // 
+            // tbComida
+            // 
+            this.tbComida.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbComida.Location = new System.Drawing.Point(155, 312);
+            this.tbComida.Name = "tbComida";
+            this.tbComida.Size = new System.Drawing.Size(199, 35);
+            this.tbComida.TabIndex = 3;
+            // 
+            // tbOutros
+            // 
+            this.tbOutros.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbOutros.Location = new System.Drawing.Point(155, 403);
+            this.tbOutros.Name = "tbOutros";
+            this.tbOutros.Size = new System.Drawing.Size(199, 35);
+            this.tbOutros.TabIndex = 2;
+            // 
+            // tbEnergia
+            // 
+            this.tbEnergia.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbEnergia.Location = new System.Drawing.Point(155, 39);
+            this.tbEnergia.Name = "tbEnergia";
+            this.tbEnergia.Size = new System.Drawing.Size(199, 35);
+            this.tbEnergia.TabIndex = 0;
+            // 
+            // tbRenda
+            // 
+            this.tbRenda.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbRenda.Location = new System.Drawing.Point(144, 51);
+            this.tbRenda.Name = "tbRenda";
+            this.tbRenda.Size = new System.Drawing.Size(258, 31);
+            this.tbRenda.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Renda familiar:";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalc.Location = new System.Drawing.Point(31, 655);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(112, 56);
+            this.btnCalc.TabIndex = 3;
+            this.btnCalc.Text = "Calcular";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(169, 655);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(112, 56);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Novo Cálculo";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(307, 655);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(112, 56);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.Text = "Fechar";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(80, 746);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(177, 25);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Total dos gastos:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(80, 818);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(119, 25);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Saldo final:";
+            // 
+            // lblSaldo
+            // 
+            this.lblSaldo.AutoSize = true;
+            this.lblSaldo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSaldo.Location = new System.Drawing.Point(280, 818);
+            this.lblSaldo.Name = "lblSaldo";
+            this.lblSaldo.Size = new System.Drawing.Size(87, 25);
+            this.lblSaldo.TabIndex = 13;
+            this.lblSaldo.Text = "R$ 0,00";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(280, 746);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(87, 25);
+            this.lblTotal.TabIndex = 14;
+            this.lblTotal.Text = "R$ 0,00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 883);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblSaldo);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gbGastos);
+            this.Controls.Add(this.tbRenda);
+            this.Name = "Form1";
+            this.Text = "Renda Familiar!!!!!!!!!!!!!!!";
+            this.gbGastos.ResumeLayout(false);
+            this.gbGastos.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbGastos;
+        private System.Windows.Forms.TextBox tbEnergia;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbTv;
+        private System.Windows.Forms.TextBox tbAgua;
+        private System.Windows.Forms.TextBox tbComida;
+        private System.Windows.Forms.TextBox tbOutros;
+        private System.Windows.Forms.TextBox tbRenda;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblSaldo;
+        private System.Windows.Forms.Label lblTotal;
+    }
+}
+
---
+++ Winformsgastos/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Winformsgastos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            double energia = double.Parse(tbEnergia.Text);
+            double agua = double.Parse(tbAgua.Text);
+            double tv = double.Parse(tbTv.Text);
+            double alimentos = double.Parse(tbComida.Text);
+            double outros = double.Parse(tbOutros.Text);
+
+            double renda = double.Parse(tbRenda.Text);
+
+             double gastos = energia + agua + tv + alimentos +outros ;
+            double saldo = renda - gastos;
+
+            lblTotal.Text = gastos.ToString("C");
+            lblSaldo.Text = saldo.ToString("C");
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            tbEnergia.Clear();
+            tbAgua.Clear();
+            tbTv.Clear();
+            tbComida.Clear();
+            tbOutros.Clear();
+            tbRenda.Clear();
+            lblSaldo.Text = "R$ 0,00";
+            lblTotal.Text = "R$ 0,00";
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
